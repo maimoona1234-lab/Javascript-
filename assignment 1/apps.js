76080dda4f271ee2f16b30cc889ad01b10c20322
maimoona1234-lab <fhina2510@gmail.com>
2026-02-21 13:11:37 +0500

    push

---
+++ assignment 1/apps.js
@@ -0,0 +1,17 @@
+// QUESTION 1
+// alert("Greeting");
+
+// QUESTION 2
+// alert("Welcome to our website");
+
+// QUESTION 3
+// alert("Welcome to Js land...\n Happy Coding!");
+
+// QUESTION 4
+// alert("Welcome to JS Land");
+
+// QUESTION 5
+// alert("Hello...I can run js through my web browser's console");
+
+
+
