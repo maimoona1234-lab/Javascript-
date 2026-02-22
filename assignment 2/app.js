36223dfa226af8dde8922a1f17bacc68a0e2c89c
fhina2510@gmail.com <alishu432@gmail.com>
2026-02-22 14:06:18 +0500

    push

---
+++ assignment 2/app.js
@@ -0,0 +1,54 @@
+// question # 1
+
+var userName;
+
+// question # 2
+
+var myName = "Maimoona Farhan"
+
+// question # 3
+// a)
+
+var message
+
+// b)
+
+message = "Hello world"
+
+// c)
+
+alert(message)
+
+// question # 4
+
+var studentName = "Maimoona Farhan";
+var studentAge = 24;
+var studentCourse = "Modern Web and App Development"
+
+alert(studentName);
+alert(studentAge)
+alert(studentCourse)
+
+// question # 5
+
+var pizza = "PIZZA \n PIZZ \n PIZ \n PI \n P";
+
+// question # 6
+
+let email = "fhina2510@gmail.com"
+alert("My email address is + email");
+
+// question # 7
+
+let book = "A Smarter way to learn javascript";
+alert ("I am trying to Learn  from the book" + book);
+
+// question # 8
+
+document.write("Yah! I canwrite HTML content through Javascript");
+
+// question # 9
+ let myString = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+ alert(myString);
+ document.write(myString);
+
